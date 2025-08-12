d5c0d4299682b9ddf087b29d42d3a34df6ec34eb
Aditya Bhaumik <92214013+aditya-bhaumik@users.noreply.github.com>
2025-08-12 16:19:40 +0530

    Not most optimized solution

---
+++ Day 1 - 12-08-2025/Palindrome Number(Leetcode)/solution_1.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String temp = Integer.toString(x);
+        StringBuilder sb = new StringBuilder(temp).reverse();
+        if(temp.equals(sb.toString())){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
+
+
+// Given an integer x, return true if x is a palindrome, and false otherwise.
+
+ 
+
+// Example 1:
+
+// Input: x = 121
+// Output: true
+// Explanation: 121 reads as 121 from left to right and from right to left.
+// Example 2:
+
+// Input: x = -121
+// Output: false
+// Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+// Example 3:
+
+// Input: x = 10
+// Output: false
+// Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
